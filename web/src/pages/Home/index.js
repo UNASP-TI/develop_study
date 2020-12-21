54066f1ad5621e778db92fac6cb28a2e87b6e24b
eliasall3x <elliasallex@gmail.com>
2020-12-21 12:56:35 -0300

    feat: added Styled-components

---
+++ web/src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// STYLES
+import { Container } from './styles';
+
+const Home = () => {
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
+
+export default Home;
