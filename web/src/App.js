49285cd22467b9815f9e83b084b762320f650684
eliasall3x <elliasallex@gmail.com>
2020-12-25 18:51:44 -0300

    RSLV: Failed to load config "react-app" to extend from.

--- web/src/App.js
+++ web/src/App.js
@@ -12,4 +12,4 @@ const App = () => (
   </>
 )
 
-export default src;
+export default App;
--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -5,7 +5,7 @@ import { Container } from './styles';
 const Home = () => {
   return (
     <Container>
-      
+      <h1>elias</h1>
     </Container>
   );
 }
