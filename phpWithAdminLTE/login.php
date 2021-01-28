2acafeb5b51af0a34f7b3f369de56e77dd6de419
Alciomar Hollanda <alciomarhollanda@gmail.com>
2021-01-27 23:13:41 -0300

    Adicionando login no admin LTE

---
+++ phpWithAdminLTE/login.php
@@ -0,0 +1,57 @@
+<?php
+include'connect.php';
+if(isset($_POST['sub'])){
+    $u=$_POST['user'];
+    $p=$_POST['pass'];
+    $s= "select * from pessoa where username='$u' and password= '$p'";   
+   $qu= mysqli_query($con, $s);
+   if(mysqli_num_rows($qu)>0){
+      $f= mysqli_fetch_assoc($qu);
+      $_SESSION['id']=$f['id'];
+      header ('location:home.php');
+   }
+   else{
+       echo 'username or password does not exist';
+   }
+}else if (isset($_POST['reg'])){
+    header ('location:reg.php');
+}
+?>
+<html>
+      
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form method="POST" enctype="multipart/form-data">
+            <table>
+                
+                <tr>
+                    <td>
+                        Username
+                        <input type="text" name="user">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        password
+                        <input type="password" name="pass">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" name="sub" value="submit">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <input type="submit" name="reg" value="cadastro">
+                         
+                    </td>
+                </tr>
+               
+            </table>
+    </body>
+</html>
