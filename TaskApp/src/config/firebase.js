568b0fcb23f7a539e6ee839d543807daff13fa6a
Alciomar Hollanda <alciomarhollanda@gmail.com>
2021-12-12 17:23:36 -0300

    App rodando perfeitamente

--- TaskApp/src/config/firebase.js
+++ TaskApp/src/config/firebase.js
@@ -1,8 +1,8 @@
+// v9 compat packages are API compatible with v8 code
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-import * as firebase from 'firebase/app';
-import 'firebase/storage';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
- 
 const firebaseConfig = {
     apiKey: "AIzaSyCXSKN_RUx00BpfIjDFDkXAKNZ3lun5TsQ",
     authDomain: "taskapp-e39f6.firebaseapp.com",
@@ -11,8 +11,8 @@ const firebaseConfig = {
     messagingSenderId: "768480291355",
     appId: "1:768480291355:web:2a716bb81bf57fe5aae6be"
   };
+  
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-export default db;
+const database = firebase.firestore();
+export default database
